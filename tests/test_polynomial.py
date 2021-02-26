ea3c81c9a1ec74d365653ce9a184da78ecb0d3d3
zhouzihan <zihan.zhou19@imperial.ac.uk>
2021-02-26 15:02:48 +0800

    .

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -2,3 +2,20 @@ from polynomials import Polynomial
 def test_print():
     p = Polynomial((2,1,0,3))
     assert str(p) == "3x^3 + x + 2"
+def test_equality():
+    assert Polynomial((0,1)) == Polynomial((0,1))
+@pytest.mark.parametrize(
+    "a, b, sum",
+    (((0,), (0, 1), (0, 1)),
+     ((2, 0 ,3), (1, 2), (3, 2, 3)),
+     ((4, 2), (10, 2, 4), (14, 4, 4)))
+    )
+def test_add(a, b, sum):
+    assert Polynomial(a) + Polynomial(b)== Polynomial(sum)
+def test_add_scalar():
+    assert Polynomial((2, 1) +3 ) == Polynomial((5,1))
+def test_reverse_add_scalar():
+    assert 3+Polynomial((2,1)) == Polynomial((5,1))
+def test_add_unknown():
+    with pytest.raises(TypeError):
+        Polynomial((1,)) + "frog"
